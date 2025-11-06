146d9596f09d931bf643904f5d598d7f2d25cd27
adr1ann32323 <adr1ann32323@gmail.com>
2025-11-06 11:58:22 -0500

    feat: project with maven

---
+++ crudcloud-backend/src/main/java/com/crudzaso/crudcloud_backend/CrudcloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crudzaso.crudcloud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcloudBackendApplication.class, args);
+	}
+
+}
