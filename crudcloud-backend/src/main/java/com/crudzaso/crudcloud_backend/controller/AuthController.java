d815d2b0912893bff1eb441e4db32d055948c271
adr1ann32323 <adr1ann32323@gmail.com>
2025-11-07 17:38:30 -0500

    Initial backend configuration with login and JWT--

---
+++ crudcloud-backend/src/main/java/com/crudzaso/crudcloud_backend/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.crudzaso.crudcloud_backend.controller;
+
+
+import com.crudzaso.crudcloud_backend.dto.LoginRequest;
+import com.crudzaso.crudcloud_backend.dto.LoginResponse;
+import com.crudzaso.crudcloud_backend.model.User;
+import com.crudzaso.crudcloud_backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Auth", description = "Authentication endpoints")
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Operation(summary = "Register a new user")
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        User savedUser = authService.register(user);
+        return ResponseEntity.ok(savedUser);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+
+}
---
+++ crudcloud-backend/src/main/java/com/crudzaso/crudcloud_backend/model/User.java
@@ -0,0 +1,29 @@
+package com.crudzaso.crudcloud_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // "ADMIN" - "USER"
+
+    @Column(nullable = false)
+    private String fullName;
+}
